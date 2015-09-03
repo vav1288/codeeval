0c6e828ac29345fc5ebf7169eab886e05da5a720
vav1288 <vav1288@users.noreply.github.com>
2015-09-03 12:14:02 -0400

    changed to match python 3 workflow

--- 186_easy_Max_Range_Sum/python2.py
+++ 186_easy_Max_Range_Sum/python2.py
@@ -9,14 +9,14 @@ with open(sys.argv[1], 'r') as test_cases:
         if len(numbers) < N:
             print(0)
             continue
-        best_sum = sum(numbers[:N])
-        current_sum = best_sum
         
-        if best_sum < 0:
-            best_sum = 0
+        current_sum = sum(numbers[:N])
+        best_sum = max(0, current_sum)
 
         for i in range(N, len(numbers)):
             current_sum += numbers[i] - numbers[i-N]
             if current_sum > best_sum:
                 best_sum = current_sum
+                
         print(best_sum)
+        
